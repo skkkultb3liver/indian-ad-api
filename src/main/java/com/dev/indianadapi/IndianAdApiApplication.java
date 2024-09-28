6ff3958ca209024992f3e5825ff3d71bbfe14244
shyninon13ave <vampirexxxwag@gmail.com>
2024-09-28 12:19:23 +0300

    init commit

---
+++ src/main/java/com/dev/indianadapi/IndianAdApiApplication.java
@@ -0,0 +1,13 @@
+package com.dev.indianadapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndianAdApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndianAdApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/indianadapi/IndianAdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.indianadapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndianAdApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
